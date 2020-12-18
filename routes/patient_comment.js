ffcfdf327882bc2382b036ba82fd0ed83dd00e03
Yami <31057849+siontama@users.noreply.github.com>
2020-12-19 02:40:18 +0900

    상단바

--- routes/patient_comment.js
+++ routes/patient_comment.js
@@ -5,12 +5,13 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 //template 파일을 이용해서 필요한 body값을 넣어주면 공통적인 html 코드를 자동 생성
 var template = require('../lib/template.js');
+var page = require('../lib/page.js')
 
 //주간 환자 상태 체크 코멘트
 
 router.get('/:patientId', function (request, response) {
     var title = 'index';
-    var html = template.HTML(title,
+    var html = page.HTML(title,
         `
         <div class="calendar"></div>
         <form class="calendar-form">
